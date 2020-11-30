744900c6b714698645a72054e5fbfa51511e3385
grandcanyonsmith <canyon@bottega.tech>
2020-11-30 00:36:17 -0700

    asdf

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css'; 
+import { Dropdown, DropdownButton, ButtonGroup, Button } from 'react-bootstrap'
+import React from 'react';
+import Navigation from '../src/Components/DollarInput';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-grid-row">
+        <div></div>
+        <div className="page-grid-column">
+      <div>Expense Tracker</div>
+      <div>Add an Expense</div>
+      <div className="expense-options">
+        <div>
+        <DropdownButton id="dropdown-item-button" title="Dropdown button">
+          <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+          <Dropdown.Item as="button">Action</Dropdown.Item>
+          <Dropdown.Item as="button">Another action</Dropdown.Item>
+          <Dropdown.Item as="button">Something else</Dropdown.Item>
+        </DropdownButton>
+        </div>
+        <div>
+        <DropdownButton id="dropdown-item-button" title="Dropdown button">
+          <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+          <Dropdown.Item as="button">Action</Dropdown.Item>
+          <Dropdown.Item as="button">Another action</Dropdown.Item>
+          <Dropdown.Item as="button">Something else</Dropdown.Item>
+      </DropdownButton>
+        </div>
+        <div>
+        <DropdownButton id="dropdown-item-button" title="Dropdown button">
+          <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+          <Dropdown.Item as="button">Action</Dropdown.Item>
+          <Dropdown.Item as="button">Another action</Dropdown.Item>
+          <Dropdown.Item as="button">Something else</Dropdown.Item>
+      </DropdownButton>
+        </div>
+        <div>
+        <DropdownButton id="dropdown-item-button" title="Dropdown button">
+          <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+          <Dropdown.Item as="button">Action</Dropdown.Item>
+          <Dropdown.Item as="button">Another action</Dropdown.Item>
+          <Dropdown.Item as="button">Something else</Dropdown.Item>
+      </DropdownButton>
+      <Button>
+        Add
+      </Button>
+        </div>
+      </div>
+    </div>
+    <div></div>
+    </div>
+    <Navigation></Navigation>
+    </div>
+
+  );
+}
+
+export default App;
